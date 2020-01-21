7116529b1fd5c5c57eb236aa99e01aba8d85667d
Burke Holland <burkeholland@gmail.com>
2020-01-21 09:24:56 -0600

    Fix collection creation statement

--- CREATE_DATABASE.sh
+++ CREATE_DATABASE.sh
@@ -1,30 +1,24 @@
 #!/bin/bash
 rnd=$RANDOM
-groupName=tailwind-products-manager
 accountName=tailwind-traders-$RANDOM
 databaseName=tailwind
 containerName=products
-
-echo "This is your concierge tenant id. Please copy this to the clipboard and press the [Enter] key to continue."
-az account list --query "[?name=='Concierge Subscription'].tenantId" -o tsv
-
-read -p  ""
 
 echo "Beginning database creation process..."
 
 groupName=$(az group list --query "[0].name" -o tsv)
 
 echo "Creating Cosmos DB database $accountName in Resource Group $groupName..."
-echo "This can take up to 10 minutes. That's the perfect amount of time to watch this YouTube video: https://youtu.be/OzKk4Wfnz1k"
+echo "This can take up to 10 minutes. Feel free to continue with the Learn Module. Just make sure to keep this terminal running."
 az cosmosdb create -n $accountName -g $groupName -o none
 
 echo "Creating 'tailwind' database in $accountName..."
-az cosmosdb sql database create --a $accountName -g $groupName -n $databaseName -o none
+az cosmosdb sql database create -a $accountName -g $groupName -n $databaseName -o none
 
 echo "Creating 'products' collection in 'tailwind' database..."
-az cosmosdb sql container create -g $groupName -n $accountName -c $containerName -d $databaseName -o none
+az cosmosdb sql container create -g $groupName -a $accountName -d $databaseName -n $containerName -p /brand/name -o none
 
-echo "Finished scaffolding database"
+echo "Preparing to import data..."
 
 endpoint=https://$accountName.documents.azure.com:443
 key=$(az cosmosdb list-keys -g $groupName -n $accountName --query "primaryMasterKey" -o json)
